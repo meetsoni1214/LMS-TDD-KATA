de4c21485b503bd5216598dd7808bf46ee822e70
Meet <meetsoni1214@gmail.com>
2024-08-31 16:32:53 +0530

    Created a List of available Books in the Library

--- src/main/java/org/example/LibraryManagementSystem.java
+++ src/main/java/org/example/LibraryManagementSystem.java
@@ -1,5 +1,17 @@
 package org.example;
 
+import org.example.model.Book;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LibraryManagementSystem {
+    private static final List<Book> availableBooks = new ArrayList<>();
+
+    // Declaring this methods so that other classes can only access the list in a read-only manner
+    public static List<Book> getAvailableBooks() {
+        return Collections.unmodifiableList(availableBooks);
+    }
 
 }
