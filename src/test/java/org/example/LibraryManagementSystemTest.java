be0f29f9120b685ae6e49d6b8e4f26f047071834
Meet <meetsoni1214@gmail.com>
2024-08-31 16:27:39 +0530

    Cannot resolve method getAvailableBooks()

--- src/test/java/org/example/LibraryManagementSystemTest.java
+++ src/test/java/org/example/LibraryManagementSystemTest.java
@@ -4,11 +4,14 @@ import org.example.model.Book;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryManagementSystemTest {
 
     LibraryManagementSystem lms;
+    public static final List<Book> availableBooks = LibraryManagementSystem.getAvailableBooks();
 
     // to get the instance of the class before running each unit test
     @BeforeEach
@@ -19,6 +22,8 @@ class LibraryManagementSystemTest {
     @Test
     public void addBookTest() {
         Book book = new Book("title", "987-123-123-9876", "author", 2004);
+        // number of books before adding
+        int noOfBooks = availableBooks.size();
     }
 
 }
